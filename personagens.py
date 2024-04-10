4ff05c2948d0c9fe9e6c4c08bf92996d4ddd4d57
Guaranaeco <viniciusf.oliv@gmail.com>
2024-04-10 17:35:38 -0300

    update cor

---
+++ personagens.py
@@ -0,0 +1,83 @@
+import random
+escolha = 0
+class Player:
+    def __init__(self, nome, vida_max, vida, ataque, level, exp, exp_max) -> None:
+        self.nome = nome
+        self.vida_max = vida_max
+        self.vida = vida
+        self.ataque = ataque
+        self.level = level
+        self.exp = exp
+        self.exp_max = exp_max
+    
+    def __str__(self) -> str:
+        return('-- Status base --\n'
+          f'{cor(96,'Nome')}: {self.nome}\n'
+               f'{cor(96,'Vida')}: {self.vida}/{self.vida_max}\n'
+               f'{cor(96,'Ataque')}: {self.ataque}\n'
+               f'{cor(96,'Level')} {self.level}\n'
+               f'{cor(96,'EXP')}: {self.exp}/{self.exp_max}')
+    
+    def battle(self, inimigo):
+        inimigo.vida -= self.ataque
+        print(f'Você ataca o inimigo! \n'
+              f'Com um corte limpo, você causa {jogador.ataque} de dano!\n'
+              f'Vida atual do inimigo {inimigo.vida}')
+ 
+class Monstro:
+    def __init__(self, nome, vida_max, vida, ataque, level, exp) -> None:
+        self.nome = nome
+        self.vida_max = vida_max
+        self.vida = vida
+        self.ataque = ataque
+        self.level = level
+        self.exp = exp
+
+
+def cor(cor, text):
+    return f'\033[{cor}m{text}\033[m'
+
+
+
+def menu():
+    while True:
+        print('1 - Atacar'
+              '2 - Status')
+        if escolha == 1:
+            heroi.battle(monstro_escolhido)
+            break
+        elif escolha == 2:
+            print(heroi)
+            break
+        else:
+            print("Opção inválida!")
+
+def heroi():
+    print(arqueiro)
+    print(barbaro)
+    print(mago)
+    escolha = int(input('Escolha seu herói: '))
+    if escolha == 1:
+        return arqueiro
+    elif escolha == 2:
+        return barbaro
+    elif escolha == 3:
+        return mago
+
+arqueiro = Player('Legolas', 20, 20, 8, 1, 0, 20)
+barbaro = Player('Conan', 30, 30, 5, 1, 0, 20)
+mago = Player('Donald', 15, 15, 11, 1, 0, 20)
+
+# Criação de instâncias de monstros
+morcego = Monstro('morcego', 5, 5, 2, 1, 5)
+esqueleto = Monstro('esqueleto', 20, 20, 5, 1, 10)
+minotauro = Monstro('minotauro', 30, 30, 10, 1, 20)
+
+# Lista de monstros com suas porcentagens de aparecimento
+lista_itens = [[morcego, 0.01], [esqueleto, 0.50], [minotauro, 0.49]]
+nomes, porcentagens = zip(*lista_itens)
+monstro_escolhido = random.choices(nomes, weights=porcentagens, k=1)[0]
+
+jogador = heroi()
+print(jogador.battle(monstro_escolhido))
+print(jogador)
