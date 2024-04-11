6c54c575b926a01e1066c3797fddb13119b00b59
Guaranaeco <viniciusf.oliv@gmail.com>
2024-04-10 22:35:19 -0300

    update battle

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
 import personagens
-import random
-import sys
-import time
+print('Aventura teste começando...')
+print(f'Em uma breve história, monstros aos redores da cidade estão atacando os viajantes, e você foi contratado para elimina-los e chegar até o lider deles, comece!\n')
 
-def palavra(texto, intervalo=0.05):
-    for char in texto:
-        if char == '\n':
-            sys.stdout.write(char)
-        else:
-            sys.stdout.write(char)
-            sys.stdout.flush()
-            time.sleep(intervalo)
 
-palavra('Aventura teste começando...')
-palavra(f'Em uma breve história, monstros aos redores da cidade estão atacando os viajantes, e você foi contratado para elimina-los e chegar até o lider deles, comece!\n')
-
-palavra('Bem vindo! \nVocê está prestes a sair da cidade, qual direção deseja tomar?\n'
+print(f'Bem vindo! {personagens.jogador.nome}\nVocê está prestes a sair da cidade, qual direção deseja tomar?\n'
       '1 - Leste\n'
       '2 - Norte\n')
 escolha = int(input('Opção desejada: '))
 
 if escolha == 1:
-    palavra('Você se aproxima do portão leste e sente cheiro de sangue...\n')
-    palavra('O cheiro se intensifica conforme você anda... barulhos estranhos...\n')
-    palavra(f'Você encontrou um {personagens.monstro_escolhido.nome}, o que fazer?\n')
-    palavra('1 - Atacar\n'
+    print('Você se aproxima do portão leste e sente cheiro de sangue...\n')
+    print('O cheiro se intensifica conforme você anda... barulhos estranhos...\n')
+    print(f'Você encontrou um {personagens.monstro_escolhido.nome}, o que fazer?\n')
+    print('1 - Atacar\n'
             '2 - Fugir\n')
     escolha = int(input('Opção desejada: '))
     if escolha == 1:
--- personagens.py
+++ personagens.py
@@ -1,5 +1,7 @@
-import random
+from random import randint, choices
 escolha = 0
+
+# Declarando classe principal do jogador
 class Player:
     def __init__(self, nome, vida_max, vida, ataque, level, exp, exp_max) -> None:
         self.nome = nome
@@ -10,6 +12,7 @@ class Player:
         self.exp = exp
         self.exp_max = exp_max
     
+    # Status do jogador
     def __str__(self) -> str:
         return('-- Status base --\n'
           f'{cor(96,'Nome')}: {self.nome}\n'
@@ -18,12 +21,16 @@ class Player:
                f'{cor(96,'Level')} {self.level}\n'
                f'{cor(96,'EXP')}: {self.exp}/{self.exp_max}')
     
+    # Método de batalha do jogador
     def battle(self, inimigo):
-        inimigo.vida -= self.ataque
+        dano = randint(int(self.ataque * 0.60), int(self.ataque * 1.20))
+        inimigo.vida -= dano
+        print(inimigo.nome)
         print(f'Você ataca o inimigo! \n'
-              f'Com um corte limpo, você causa {jogador.ataque} de dano!\n'
+              f'Com um corte limpo, você causa {dano} de dano!\n'
               f'Vida atual do inimigo {inimigo.vida}')
  
+ # Declarando classe principal dos monstros
 class Monstro:
     def __init__(self, nome, vida_max, vida, ataque, level, exp) -> None:
         self.nome = nome
@@ -33,12 +40,19 @@ class Monstro:
         self.level = level
         self.exp = exp
 
+    # Método de batalha do monstro
+    def battle(self, jogador):
+        jogador.vida -= self.ataque
+        print(f'Monstro ataca o jogador! \n'
+              f'Com um corte limpo, você causa {self.ataque} de dano!\n'
+              f'Vida atual do {jogador.nome} {jogador.vida}')
 
+# Função para colorir texto.
 def cor(cor, text):
     return f'\033[{cor}m{text}\033[m'
 
 
-
+# Menu do jogador a cada jogada.
 def menu():
     while True:
         print('1 - Atacar'
@@ -52,6 +66,7 @@ def menu():
         else:
             print("Opção inválida!")
 
+# Escolhendo personagem para a campanha
 def heroi():
     print(arqueiro)
     print(barbaro)
@@ -64,20 +79,33 @@ def heroi():
     elif escolha == 3:
         return mago
 
+
+# Instâncias de classes
 arqueiro = Player('Legolas', 20, 20, 8, 1, 0, 20)
 barbaro = Player('Conan', 30, 30, 5, 1, 0, 20)
 mago = Player('Donald', 15, 15, 11, 1, 0, 20)
 
-# Criação de instâncias de monstros
+# Instâncias de monstros
 morcego = Monstro('morcego', 5, 5, 2, 1, 5)
 esqueleto = Monstro('esqueleto', 20, 20, 5, 1, 10)
 minotauro = Monstro('minotauro', 30, 30, 10, 1, 20)
 
-# Lista de monstros com suas porcentagens de aparecimento
-lista_itens = [[morcego, 0.01], [esqueleto, 0.50], [minotauro, 0.49]]
+# Lista de monstros com suas porcentagens de aparecimento (Até o momento)
+lista_itens = [[Monstro('morcego', 5, 5, 2, 1, 5), 0.33], [Monstro('esqueleto', 20, 20, 5, 1, 10), 0.33], [Monstro('minotauro', 30, 30, 10, 1, 20), 0.33]]
 nomes, porcentagens = zip(*lista_itens)
-monstro_escolhido = random.choices(nomes, weights=porcentagens, k=1)[0]
+monstro_escolhido = choices(nomes, weights=porcentagens, k=1)[0]
 
+
+# TESTANDO
 jogador = heroi()
+'''print(f'Você achou um {monstro_escolhido.nome}')
+print('---------------')
 print(jogador.battle(monstro_escolhido))
-print(jogador)
+print('---------------')
+print(monstro_escolhido.battle(jogador))
+'''
+while True:
+    if monstro_escolhido.vida > 0:
+        jogador.battle(monstro_escolhido)
+    else:
+        monstro_escolhido = choices(nomes, weights=porcentagens, k=1)[0]
